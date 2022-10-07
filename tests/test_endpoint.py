8282cf0b8fd65978f0f3acd673b28107617f4b26
Garrett Madsen <garrett.l.madsen@gmail.com>
2022-10-07 07:35:07 -0400

    working test

--- tests/test_endpoint.py
+++ tests/test_endpoint.py
@@ -1,13 +1,14 @@
 import pytest
+import pytest_asyncio
 from  movieapp.movieapp import MOVIE_APP as app
 
 
-@pytest.fixture(name='test_app')
+@pytest_asyncio.fixture(name='test_app')
 def _test_app():
     return app
 
 @pytest.mark.asyncio
-async def test_app(test_app):
+async def test_routes(test_app):
     """ basic test """
     client = test_app.test_client()
     response = await client.get('/')
